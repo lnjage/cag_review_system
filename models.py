20ef55aa6e9ee2d95367035cf121efa2157d44a4
lnjage <lnjanja@outlook.com>
2025-04-10 16:32:31 +0300

    capitalization fix

--- models.py
+++ models.py
@@ -5,21 +5,21 @@ db = SQLAlchemy () #setup databse engine
 
 #usermode reviewer/admim
 
-class User(UserMixin, db.model):
-    id=db.Column(db.interger, primary_key=True)
-    username=db.column(db.string(50), unique=True,nullable=False)
-    password=db.column(db.string(100), nullable= False)
-    is_admin=db.column(db.boolean, nullable=False)
-    subjects=db.column(db.string(200))
+class User(UserMixin, db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(50), unique=True,nullable=False)
+    password=db.Column(db.String(100), nullable= False)
+    is_admin=db.Column(db.Boolean, nullable=False)
+    subjects=db.Column(db.String(200))
 
 
 #abstract model
 
-class Abstract(db.model):
-    id=db.Column()(db.interger, primary_key=True)
-    email=db.Column(db.string(50), nullable=False)
-    subject=db.Column(db.string(150), nullable=False)
-    content=db.column(db.string(500),nullable=False)
+class Abstract(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    email=db.Column(db.String(50), nullable=False)
+    subject=db.Column(db.String(150), nullable=False)
+    content=db.Column(db.String(500),nullable=False)
 
 #review model
 
